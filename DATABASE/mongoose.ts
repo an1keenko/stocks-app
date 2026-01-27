e6a64c9395d88958bd46afff0e48038b06d5e677
Ihar Anikeyenka <ihar.anikeyenka@n1affiliates.com>
2026-01-27 22:37:23 +0100

    feat: Add mongoose configuration and dependencies for MongoDB integration

---
+++ DATABASE/mongoose.ts
@@ -0,0 +1,16 @@
+import * as mongoose from 'mongoose';
+
+const MOGODB_URI = process.env.MONGODB_URI;
+
+declare global {
+  var mongooseCache: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
+}
+
+let cached = global.mongooseCache;
+
+if (!cached) {
+  cached = global.mongooseCache = { conn: null, promise: null };
+}
