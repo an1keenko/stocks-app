b9a5b71ea72cf226978f2190bc8175f7cad5cde2
Ihar Anikeyenka <ihar.anikeyenka@n1affiliates.com>
2026-01-28 08:57:14 +0100

    feat: Add database connection utility and dotenv support for MongoDB

--- DATABASE/mongoose.ts
+++ DATABASE/mongoose.ts
@@ -1,6 +1,6 @@
 import * as mongoose from 'mongoose';
 
-const MOGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 declare global {
   var mongooseCache: {
@@ -14,3 +14,22 @@ let cached = global.mongooseCache;
 if (!cached) {
   cached = global.mongooseCache = { conn: null, promise: null };
 }
+
+export const connectToDatabase = async () => {
+  if (!MONGODB_URI) throw new Error('MONGODB_URI must be set within .env file');
+
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (err) {
+    cached.promise = null;
+    throw err;
+  }
+
+  console.log(`Connected to database ${process.env.NODE_ENV} ${MONGODB_URI}`);
+};
