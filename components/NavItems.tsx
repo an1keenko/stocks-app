5e95f4669b01aa612b45a9305c078bda2df290ca
Ihar Anikeyenka <ihar.anikeyenka@n1affiliates.com>
2026-01-29 22:22:14 +0100

    feat: Integrate daily news summary function into route handling

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -19,7 +19,7 @@ const NavItems = ({ initialStocks }: { initialStocks: StockWithWatchlistStatus[]
   return (
     <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
       {NAV_ITEMS.map(({ href, label }) => {
-        if (label === 'Search')
+        if (href === '/search')
           return (
             <li key="search-trigger">
               <SearchCommand renderAs="text" label="Search" initialStocks={initialStocks} />
